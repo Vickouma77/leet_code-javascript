48090c2aae7dd2929bb12d5722f3a382be9135b0
Vickouma77 <Vickprestone20@gmail.com>
2025-01-27 13:17:05 +0300

    feat(main): initialize Actix-web server with routing and environment setup
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+mod models;
+mod routes;
+
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
+use routes::user_routes::init;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+
+    println!("Server running at http://{}:{}", host, port);
+
+    HttpServer::new(|| {
+        App::new()
+            .configure(init)
+    })
+    .bind(&format!("{}:{}", host, port))?
+        .run()
+    .await
 }
