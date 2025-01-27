e49e0a217325121bb10c6aa8f12fe9fd143f8bd3
Vickouma77 <Vickprestone20@gmail.com>
2025-01-27 13:05:07 +0300

    feat(routes): registering routes for the api
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

---
+++ rust-api/src/routes/user_routes.rs
@@ -0,0 +1,12 @@
+use actix_web::web;
+
+use crate::handlers::user_handlers::{create_user, get_user, health_check};
+
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api/v1")
+            .route("/users", web::post().to(create_user))
+            .route("/users/{id}", web::get().to(get_user))
+            .route("/health", web::get().to(health_check)),
+    );
+}
